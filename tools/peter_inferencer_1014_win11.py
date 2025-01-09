5ce52ed7a4c31b611a12b10c700635c15ba369a5
Jianran Zhang <peterstark1900@outlook.com>
2025-01-09 15:37:29 +0800

    对json可视化进行初步封装： 	修改：     tools/peter_inferencer_1014_win11.py

--- tools/peter_inferencer_1014_win11.py
+++ tools/peter_inferencer_1014_win11.py
@@ -385,125 +385,80 @@ class FishDetector():
         # 导出frame_stamps
         self.export_frame_stamps()
 
-# class DataVisualizer():
-#     def __init__(self, frame_stamps_path):
-#         self.frame_stamps_path = frame_stamps_path
-#         self.data = None
-#     def load_frame_stamps(self):
-#         with open('frame_stamps.json', 'r') as f:
-#         self.data = json.load(f)
+class Visualizer():
+    def __init__(self, file_path,file_type):
+        self.file_type = file_type
+        if self.file_type == 'json':
+            self.file_path = file_path
+            with open(self.file_path, 'r') as f:
+                self.data = json.load(f)
+    
+    def show_animation(self):
+        # 创建一个空白窗口
+        cv2.namedWindow('Keypoints Visualization', cv2.WINDOW_NORMAL)
+
+        for frame_stamp in self.data['frame_stamps']:
+            # 创建一个空白图像
+            frame = np.zeros((1080, 1920, 3), dtype=np.uint8)* 255
+            head = frame_stamp['head']
+            body = frame_stamp['body']
+            joint = frame_stamp['joint']
+            tail = frame_stamp['tail']
+            # 绘制头部关键点
+            cv2.circle(frame, head, 5, (0, 255, 0), -1)
+            # 绘制身体关键点
+            cv2.circle(frame, body, 5, (0, 255, 0), -1)
+            # 绘制关节关键点
+            cv2.circle(frame, joint, 5, (0, 255, 0), -1)
+            # 绘制尾部关键点
+            cv2.circle(frame, tail, 5, (0, 0, 255), -1)
+            # 用线段连接关键点
+            cv2.line(frame, head, body, (255, 0, 0), 2)
+            cv2.line(frame, body, joint, (255, 0, 0), 2)
+            cv2.line(frame, joint, tail, (255, 0, 0), 2)
+
+            # 显示图像
+            cv2.imshow('Keypoints Visualization', frame)
+            cv2.waitKey(10)
+
+            # 清空图像
+            frame.fill(0)
+
+            # # 等待一段时间以显示每一帧
+            # if cv2.waitKey(500) & 0xFF == ord('q'):
+            #     break
+
+def peter_inferencer():
+    detect_type = 'video'
+    # detect_type = 'camera'
+    my_pose_cfg = r"E:\openmmlab\mmpose\configs\fish_keypoints\fish-keypoints-1222.py"
+    # my_pose_weights = r"C:\Users\peter\OneDrive\毕设\demo_video\best_coco_AP_epoch_340.pth"
+    my_pose_weights = r"E:\openmmlab\mmpose\work_dirs\fish-keypoints-1222\best_coco_AP_epoch_380.pth"
+    my_detect_cfg = r"E:\openmmlab\mmdetection\configs\fish\fish1210-rtmdet_tiny_8xb32-300e_coco.py"
+    # my_detect_weights = r"C:\Users\peter\OneDrive\毕设\demo_video\epoch_300.pth"
+    my_detect_weights = r"E:\openmmlab\mmdetection\work_dirs\fish1222-rtmdet_tiny_8xb32-300e_coco\epoch_300.pth"
+    my_kpt_thr = 0.2
+    my_real_num = 1
+    my_draw_flag = True
+    my_save_flag = False
+    input_vidoe_path = r"C:\Users\peter\OneDrive\毕设\数据集\Fish-1222\fish-1222-demo20.mp4"
+    output_path = 'opencv_demo.mp4'
+    fish_detector = FishDetector(detect_type, my_pose_cfg, my_pose_weights, my_detect_cfg, my_detect_weights, my_kpt_thr, my_real_num, my_draw_flag, my_save_flag, input_vidoe_path, output_path)
+    fish_detector.frame_pipeline()
+
+def peter_visualizer():
+    # file_path = 'frame_stamps.json'
+    file_path = 'fish-1222-demo19.json'
+    file_type = 'json'
+    visualizer = Visualizer(file_path,file_type)
+    visualizer.show_animation()
 
 def main():
-    # detect_type = 'video'
-    # # detect_type = 'camera'
-    # my_pose_cfg = r"E:\openmmlab\mmpose\configs\fish_keypoints\fish-keypoints-1222.py"
-    # # my_pose_weights = r"C:\Users\peter\OneDrive\毕设\demo_video\best_coco_AP_epoch_340.pth"
-    # my_pose_weights = r"E:\openmmlab\mmpose\work_dirs\fish-keypoints-1222\best_coco_AP_epoch_380.pth"
-    # my_detect_cfg = r"E:\openmmlab\mmdetection\configs\fish\fish1210-rtmdet_tiny_8xb32-300e_coco.py"
-    # # my_detect_weights = r"C:\Users\peter\OneDrive\毕设\demo_video\epoch_300.pth"
-    # my_detect_weights = r"E:\openmmlab\mmdetection\work_dirs\fish1222-rtmdet_tiny_8xb32-300e_coco\epoch_300.pth"
-    # my_kpt_thr = 0.2
-    # my_real_num = 1
-    # my_draw_flag = True
-    # my_save_flag = False
-    # input_vidoe_path = r"C:\Users\peter\OneDrive\毕设\数据集\Fish-1222\fish-1222-demo20.mp4"
-    # output_path = 'opencv_demo.mp4'
-    # fish_detector = FishDetector(detect_type, my_pose_cfg, my_pose_weights, my_detect_cfg, my_detect_weights, my_kpt_thr, my_real_num, my_draw_flag, my_save_flag, input_vidoe_path, output_path)
-    # fish_detector.frame_pipeline()
+    # peter_inferencer()
+    peter_visualizer()
     
-    # keypoints = [[[1121.5007, 643.6478],
-    #                 [1075.7317, 723.5621],
-    #                 [1024.8772, 720.65607],
-    #                 [986.37305, 739.5449]]]
     
-    # head = tuple(map(int, keypoints[0][0]))
-    # body = tuple(map(int, keypoints[0][1]))
-    # joint = tuple(map(int, keypoints[0][2]))
-    # tail = tuple(map(int, keypoints[0][3]))
-    # print(head)
-    # print(body)
-    # print(joint)
-    # print(tail)
-
-    # # 保存到json文件
-    # frame_stamps = []
-    # keypoint_stamp1 = {
-    #     "head": head,
-    #     "body": body,
-    #     "joint": joint,
-    #     "tail": tail
-    # }
-    # keypoint_stamp2 = {
-    #     "head": head,
-    #     "body": body,
-    #     "joint": joint,
-    #     "tail": tail
-    # }
-    # # 将keypoint_stamp1与keypoint_stamp2依次添加到frame_stamps中
-    # frame_stamps.append(keypoint_stamp1)
-    # frame_stamps.append(keypoint_stamp2)
-    # print(frame_stamps)
-    # data = {
-    #     "frame_stamps": frame_stamps
-    # }
-    # import json
-    # with open('frame_stamps.json', 'w') as f:
-    #     json.dump(data, f, indent=4)
-
-    # import json
-    with open('frame_stamps.json', 'r') as f:
-        data = json.load(f)
-    # print(data)
-    # print('---------------------------------')
-    # print(data['frame_stamps'])
-    # print('---------------------------------')
-    # print(data['frame_stamps'][0])
-    # print('---------------------------------')
-    # print(data['frame_stamps'][0]['head'])
-    # print('---------------------------------')
-
-    # 创建一个空白窗口
-    cv2.namedWindow('Keypoints Visualization', cv2.WINDOW_NORMAL)
-
-    for frame_stamp in data['frame_stamps']:
-        # 创建一个空白图像
-        frame = np.zeros((1080, 1920, 3), dtype=np.uint8)* 255
-        print(frame_stamp['head'])
-        print(frame_stamp['body'])
-        print(frame_stamp['joint'])
-        print(frame_stamp['tail'])
-        print('---------------------------------')
-        # 绘制关键点和线段
-        head = frame_stamp['head']
-        body = frame_stamp['body']
-        joint = frame_stamp['joint']
-        tail = frame_stamp['tail']
-        # 绘制头部关键点
-        cv2.circle(frame, head, 5, (0, 255, 0), -1)
-        # 绘制身体关键点
-        cv2.circle(frame, body, 5, (0, 255, 0), -1)
-        # 绘制关节关键点
-        cv2.circle(frame, joint, 5, (0, 255, 0), -1)
-        # 绘制尾部关键点
-        cv2.circle(frame, tail, 5, (0, 0, 255), -1)
-        # 用线段连接关键点
-        cv2.line(frame, head, body, (255, 0, 0), 2)
-        cv2.line(frame, body, joint, (255, 0, 0), 2)
-        cv2.line(frame, joint, tail, (255, 0, 0), 2)
-
-        # 显示图像
-        cv2.imshow('Keypoints Visualization', frame)
-        cv2.waitKey(10)
-
-        # 清空图像
-        frame.fill(0)
-
-        # # 等待一段时间以显示每一帧
-        # if cv2.waitKey(500) & 0xFF == ord('q'):
-        #     break
-
-    # 关闭所有窗口
-    cv2.destroyAllWindows()
+    
 
 
 
